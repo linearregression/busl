17c9270049dc4a5b65f5f9a5f4d1dea52fa47fa7
Naaman Newbold <naaman@heroku.com>
2014-06-05 11:08:05 -0700

    Cleaner pool logic

--- redis.go
+++ redis.go
@@ -23,20 +23,26 @@ func newPool(server *url.URL) *redis.Pool {
 	return &redis.Pool{
 		MaxIdle: 3,
 		IdleTimeout: 4 * time.Minute,
-		Dial: func () (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server.Host)
+		Dial: func () (c redis.Conn, err error) {
+			c, err = redis.Dial("tcp", server.Host)
 			if err != nil {
-				return nil, err
+				return
 			}
-			if server.User != nil {
-				if pw, _ := server.User.Password(); pw != "" {
-					if _, err := c.Do("AUTH", pw); err != nil {
-						c.Close()
-						return nil, err
-					}
-				}
+
+			if server.User == nil {
+				return
 			}
-			return c, err
+
+			pw, pwset := server.User.Password()
+			if !pwset {
+				return
+			}
+
+			if _, err = c.Do("AUTH", pw); err != nil {
+				c.Close()
+				return
+			}
+			return
 		},
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			_, err := c.Do("PING")
@@ -59,11 +65,11 @@ func NewRedisBroker(uuid UUID) *RedisBroker {
 	return broker
 }
 
-func (b *RedisBroker) Subscribe() chan []byte {
-	ch := make(chan []byte, msgBuf)
+func (b *RedisBroker) Subscribe() (ch chan []byte) {
+	ch = make(chan []byte, msgBuf)
 	b.subscribers[ch] = true
 	go b.redisSubscribe(ch)
-	return ch
+	return
 }
 
 func (b *RedisBroker) redisSubscribe(ch chan []byte) {
