caddf3f75c9a8a7a2ca7fd4bbbaf8ee417146c93
Troels Thomsen <troels@thomsen.io>
2014-12-12 16:28:12 -0800

    Allow toggling HTTPS enforcement

--- util/config.go
+++ util/config.go
@@ -7,6 +7,7 @@ import (
 )
 
 var (
+	EnforceHTTPS      = flag.Bool("enforceHttps", os.Getenv("ENFORCE_HTTPS") == "1", "Whether to enforce use of HTTPS.")
 	HttpPort          = flag.String("httpPort", os.Getenv("PORT"), "HTTP port for the server.")
 	HeartbeatDuration = flag.Duration("subscribeHeartbeatDuration", time.Second*10, "Heartbeat interval for HTTP stream subscriptions.")
 )
