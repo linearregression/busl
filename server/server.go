39c357c591403321046aa10c6ba1950454ff950c
Cyril David <cyril@heroku.com>
2015-03-13 18:22:11 -0700

    Move addDefaultHeaders
    It never made sense to have it in server.go when all the
    other methods with the same signature are in middleware.go

--- server/server.go
+++ server/server.go
@@ -136,18 +136,6 @@ func sub(w http.ResponseWriter, r *http.Request) {
 	io.Copy(NewWriteFlusher(w), reader)
 }
 
-func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		fn(w, r)
-	}
-}
-
 func app() http.Handler {
 	p := pat.New()
 
