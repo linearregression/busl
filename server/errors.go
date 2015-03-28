8cd7fcd38bfe1f84e25257cc831ea561c49064c7
Cyril David <cyril@heroku.com>
2015-03-28 15:09:23 -0700

    Additional check for 404

--- server/errors.go
+++ server/errors.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/heroku/busl/assets"
 	"github.com/heroku/busl/broker"
+	"github.com/heroku/busl/storage"
+	"github.com/heroku/busl/util"
 )
 
 func handleError(w http.ResponseWriter, r *http.Request, err error) {
-	if err == broker.ErrNotRegistered {
+	if err == broker.ErrNotRegistered || err == storage.ErrNoStorage {
 		message := "Channel is not registered."
 		if r.Header.Get("Accept") == "text/ascii; version=feral" {
 			message = assets.HttpCatGone
