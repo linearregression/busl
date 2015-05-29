28e1039b0e8302f27b41f4ec4d55381db5edb11d
Cyril David <cyril@heroku.com>
2015-05-29 10:59:17 -0700

    Use testify for broker/ and small reorg
    - IO related tests go to io_test
    - redis_test is now purely registrar tests

--- broker/io_test.go
+++ broker/io_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/heroku/busl/util"
@@ -18,6 +19,16 @@ func setup() string {
 	registrar.Register(uuid)
 
 	return uuid
+}
+
+func newReaderWriter() (io.ReadCloser, io.WriteCloser) {
+	registrar := NewRedisRegistrar()
+	uuid, _ := util.NewUUID()
+	registrar.Register(uuid)
+	r, _ := NewReader(uuid)
+	w, _ := NewWriter(uuid)
+
+	return r, w
 }
 
 func ExamplePubSub() {
@@ -160,4 +171,55 @@ func TestOverflowingBuffer(t *testing.T) {
 	}()
 	w.Close()
 	assert.Equal(t, int64(32769), <-done)
+}
+
+func ExampleSubscribeConcurrent() {
+	r, w := newReaderWriter()
+
+	pub := make(chan bool)
+	done := make(chan bool)
+
+	go func() {
+		pub <- true
+		w.Write([]byte("busl"))
+		w.Close()
+	}()
+
+	go func() {
+		<-pub
+		io.Copy(os.Stdout, r)
+		done <- true
+	}()
+
+	<-done
+	//Output:
+	// busl
+}
+
+func TestRedisReadFromClosed(t *testing.T) {
+	r, w := newReaderWriter()
+	p := make([]byte, 10)
+
+	r.Read(p)
+	w.Write([]byte("hello"))
+	w.Close()
+
+	// this read should short circuit with EOF
+	_, err := r.Read(p)
+	assert.Equal(t, err, io.EOF)
+
+	// We'll get true here because r.closed is already set
+	assert.True(t, ReaderDone(r))
+
+	// We should still get true here because doneId is set
+	r, _ = NewReader(string(r.(*reader).channel))
+	assert.True(t, ReaderDone(r))
+
+	// Reader done on a regular io.Reader should return false
+	// and not panic
+	assert.False(t, ReaderDone(strings.NewReader("hello")))
+
+	// NoContent should respond accordingly based on offset
+	assert.False(t, NoContent(r, 0))
+	assert.True(t, NoContent(r, 5))
 }
