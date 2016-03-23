891a1393517779f9d36fe4207a950413ffb46101
Damien Mathieu <damien@heroku.com>
2016-03-23 11:56:22 +0100

    make requests logging closer to the platform router's format

--- util/response_logger.go
+++ util/response_logger.go
@@ -7,16 +7,14 @@ import (
 )
 
 // NewResponseLogger creates a new logger for HTTP responses
-func NewResponseLogger(w http.ResponseWriter, path string, userAgent string, requestID string) *responseLogger {
-	return &responseLogger{ResponseWriter: w, status: http.StatusOK, path: path, userAgent: userAgent, requestID: requestID}
+func NewResponseLogger(r *http.Request, w http.ResponseWriter) *responseLogger {
+	return &responseLogger{ResponseWriter: w, request: r, status: http.StatusOK}
 }
 
 type responseLogger struct {
 	http.ResponseWriter
-	status    int
-	path      string
-	userAgent string
-	requestID string
+	request *http.Request
+	status  int
 }
 
 func (l *responseLogger) WriteHeader(s int) {
@@ -32,7 +30,27 @@ func (l *responseLogger) Flush() {
 	l.ResponseWriter.(http.Flusher).Flush()
 }
 
+func (l *responseLogger) requestID() (id string) {
+	if id = l.request.Header.Get("Request-Id"); id == "" {
+		id = l.request.Header.Get("X-Request-Id")
+	}
+
+	if id == "" {
+		// In the event of a rare case where uuid
+		// generation fails, it's probably more
+		// desirable to continue as is with an empty
+		// request_id than to bubble the error up the
+		// stack.
+		uuid, _ := NewUUID()
+		id = string(uuid)
+	}
+
+	return id
+}
+
 func (l *responseLogger) WriteLog() {
 	maskedStatus := strconv.Itoa(l.status/100) + "xx"
-	log.Printf("count#http.status.%s=1 status=%d path=%s user_agent=\"%s\" request_id=%s", maskedStatus, l.status, l.path, l.userAgent, l.requestID)
+	log.Printf("count#http.status.%s=1 request_id=%s", maskedStatus, l.requestID())
+	log.Printf("method=%s path=\"%s\" host=\"%s\" fwd=\"%s\" status=%d user_agent=\"%s\" request_id=%s",
+		l.request.Method, l.request.URL.Path, l.request.Host, l.request.Header.Get("X-Forwarded-For"), l.status, l.request.UserAgent(), l.requestID())
 }
