797a5d9d8067f6041996fdf1377bd29b3efeb28d
Damien Mathieu <damien@heroku.com>
2016-05-13 10:36:45 +0200

    don't use any global cli flags

--- cmd/busl/main.go
+++ cmd/busl/main.go
@@ -12,59 +12,66 @@ import (
 
 	"github.com/heroku/busl/logging"
 	"github.com/heroku/busl/server"
-	"github.com/heroku/rollbar"
 
+	"github.com/heroku/rollbar"
 	_ "github.com/joho/godotenv/autoload"
 )
 
-// global cli options
-var (
-	HTTPPort         = flag.String("httpPort", os.Getenv("PORT"), "HTTP port for the server.")
-	HTTPReadTimeout  = flag.Duration("httpReadTimeout", time.Hour, "Timeout for HTTP request reading")
-	HTTPWriteTimeout = flag.Duration("httpWriteTimeout", time.Hour, "Timeout for HTTP request writing")
+type cmdConfig struct {
+	RollbarEnvironment string
+	RollbarToken       string
 
-	RollbarEnvironment = flag.String("rollbarEnvironment", os.Getenv("ROLLBAR_ENVIRONMENT"), "Rollbar Enviornment for this application (development/staging/production).")
-	RollbarToken       = flag.String("rollbarToken", os.Getenv("ROLLBAR_TOKEN"), "Rollbar Token for sending issues to Rollbar.")
-)
+	HTTPPort         string
+	HTTPReadTimeout  time.Duration
+	HTTPWriteTimeout time.Duration
+}
 
 func main() {
 	logging.Configure()
 
 	fmt.Println("Starting busl...")
-	conf, err := parseFlags()
+	cmdConf, httpConf, err := parseFlags()
 	if err != nil {
 		os.Exit(1)
 	}
 
-	if *RollbarToken != "" {
-		rollbar.Token = *RollbarToken
-		rollbar.Environment = *RollbarEnvironment
+	if cmdConf.RollbarToken != "" {
+		rollbar.Token = cmdConf.RollbarToken
+		rollbar.Environment = cmdConf.RollbarEnvironment
 		rollbar.ServerRoot = "github.com/heroku/busl"
 	}
 
-	_, err = strconv.Atoi(*HTTPPort)
+	_, err = strconv.Atoi(cmdConf.HTTPPort)
 	if err != nil {
 		log.Printf("%s: $PORT must be an integer value.\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	s := server.NewServer(conf)
-	s.ReadTimeout = *HTTPReadTimeout
-	s.WriteTimeout = *HTTPWriteTimeout
-	s.Start(*HTTPPort, awaitSignals(syscall.SIGURG))
+	s := server.NewServer(httpConf)
+	s.ReadTimeout = cmdConf.HTTPReadTimeout
+	s.WriteTimeout = cmdConf.HTTPWriteTimeout
+	s.Start(cmdConf.HTTPPort, awaitSignals(syscall.SIGURG))
 }
 
-func parseFlags() (*server.Config, error) {
-	conf := &server.Config{}
+func parseFlags() (*cmdConfig, *server.Config, error) {
+	httpConf := &server.Config{}
+	cmdConf := &cmdConfig{}
 
-	flag.StringVar(&conf.Credentials, "creds", os.Getenv("CREDS"), "user1:pass1|user2:pass2")
-	flag.BoolVar(&conf.EnforceHTTPS, "enforceHttps", os.Getenv("ENFORCE_HTTPS") == "1", "Whether to enforce use of HTTPS.")
-	flag.DurationVar(&conf.HeartbeatDuration, "subscribeHeartbeatDuration", time.Second*10, "Heartbeat interval for HTTP stream subscriptions.")
-	flag.StringVar(&conf.StorageBaseURL, "storageBaseURL", os.Getenv("STORAGE_BASE_URL"), "Optional persistent blob storage (i.e. S3)")
+	flag.StringVar(&cmdConf.RollbarEnvironment, "rollbarEnvironment", os.Getenv("ROLLBAR_ENVIRONMENT"), "Rollbar Enviornment for this application (development/staging/production).")
+	flag.StringVar(&cmdConf.RollbarToken, "rollbarToken", os.Getenv("ROLLBAR_TOKEN"), "Rollbar Token for sending issues to Rollbar.")
+
+	flag.StringVar(&cmdConf.HTTPPort, "httpPort", os.Getenv("PORT"), "HTTP port for the server.")
+	flag.DurationVar(&cmdConf.HTTPReadTimeout, "httpReadTimeout", time.Hour, "Timeout for HTTP request reading")
+	flag.DurationVar(&cmdConf.HTTPWriteTimeout, "httpWriteTimeout", time.Hour, "Timeout for HTTP request writing")
+
+	flag.StringVar(&httpConf.Credentials, "creds", os.Getenv("CREDS"), "user1:pass1|user2:pass2")
+	flag.BoolVar(&httpConf.EnforceHTTPS, "enforceHttps", os.Getenv("ENFORCE_HTTPS") == "1", "Whether to enforce use of HTTPS.")
+	flag.DurationVar(&httpConf.HeartbeatDuration, "subscribeHeartbeatDuration", time.Second*10, "Heartbeat interval for HTTP stream subscriptions.")
+	flag.StringVar(&httpConf.StorageBaseURL, "storageBaseURL", os.Getenv("STORAGE_BASE_URL"), "Optional persistent blob storage (i.e. S3)")
 
 	flag.Parse()
 
-	return conf, nil
+	return cmdConf, httpConf, nil
 }
 
 func awaitSignals(signals ...os.Signal) <-chan struct{} {
--- cmd/busltee/main.go
+++ cmd/busltee/main.go
@@ -19,25 +19,30 @@ var (
 	RollbarToken       = flag.String("rollbarToken", os.Getenv("ROLLBAR_TOKEN"), "Rollbar Token for sending issues to Rollbar.")
 )
 
+type cmdConfig struct {
+	RollbarEnvironment string
+	RollbarToken       string
+}
+
 func main() {
 	logging.Configure()
 
-	conf, err := parseFlags()
+	cmdConf, publisherConf, err := parseFlags()
 	if err != nil {
 		usage()
 		os.Exit(1)
 	}
 
-	if *RollbarToken != "" {
-		rollbar.Token = *RollbarToken
-		rollbar.Environment = *RollbarEnvironment
+	if cmdConf.RollbarToken != "" {
+		rollbar.Token = cmdConf.RollbarToken
+		rollbar.Environment = cmdConf.RollbarEnvironment
 		rollbar.ServerRoot = "github.com/heroku/busl"
 	}
 
-	publisher.OpenLogs(conf.LogFile, conf.LogPrefix)
+	publisher.OpenLogs(publisherConf.LogFile, publisherConf.LogPrefix)
 	defer publisher.CloseLogs()
 
-	if exitCode := publisher.Run(conf.URL, conf.Args, conf); exitCode != 0 {
+	if exitCode := publisher.Run(publisherConf.URL, publisherConf.Args, publisherConf); exitCode != 0 {
 		os.Exit(exitCode)
 	}
 }
@@ -47,25 +52,29 @@ func usage() {
 	flag.PrintDefaults()
 }
 
-func parseFlags() (*publisher.Config, error) {
-	conf := &publisher.Config{}
+func parseFlags() (*cmdConfig, *publisher.Config, error) {
+	publisherConf := &publisher.Config{}
+	cmdConf := &cmdConfig{}
+
+	flag.StringVar(&cmdConf.RollbarEnvironment, "rollbarEnvironment", os.Getenv("ROLLBAR_ENVIRONMENT"), "Rollbar Enviornment for this application (development/staging/production).")
+	flag.StringVar(&cmdConf.RollbarToken, "rollbarToken", os.Getenv("ROLLBAR_TOKEN"), "Rollbar Token for sending issues to Rollbar.")
 
 	// Connection related flags
-	flag.BoolVarP(&conf.Insecure, "insecure", "k", false, "allows insecure SSL connections")
-	flag.IntVar(&conf.Retry, "retry", 5, "max retries for connect timeout errors")
-	flag.Float64Var(&conf.Timeout, "connect-timeout", 1, "max number of seconds to connect to busl URL")
+	flag.BoolVarP(&publisherConf.Insecure, "insecure", "k", false, "allows insecure SSL connections")
+	flag.IntVar(&publisherConf.Retry, "retry", 5, "max retries for connect timeout errors")
+	flag.Float64Var(&publisherConf.Timeout, "connect-timeout", 1, "max number of seconds to connect to busl URL")
 
 	// Logging related flags
-	flag.StringVar(&conf.LogPrefix, "log-prefix", "", "log prefix")
-	flag.StringVar(&conf.LogFile, "log-file", "", "log file")
-	flag.StringVar(&conf.RequestID, "request-id", "", "request id")
+	flag.StringVar(&publisherConf.LogPrefix, "log-prefix", "", "log prefix")
+	flag.StringVar(&publisherConf.LogFile, "log-file", "", "log file")
+	flag.StringVar(&publisherConf.RequestID, "request-id", "", "request id")
 
 	if flag.Parse(); len(flag.Args()) < 2 {
-		return nil, errors.New("insufficient args")
+		return nil, nil, errors.New("insufficient args")
 	}
 
-	conf.URL = flag.Arg(0)
-	conf.Args = flag.Args()[1:]
+	publisherConf.URL = flag.Arg(0)
+	publisherConf.Args = flag.Args()[1:]
 
-	return conf, nil
+	return cmdConf, publisherConf, nil
 }
